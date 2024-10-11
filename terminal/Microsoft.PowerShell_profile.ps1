102d24f18b3d5e28733b5c79d6d8ba510d9e8ee7
Daniel van Opstal <dvopstal05@gmail.com>
2024-10-11 22:14:27 +0200

    update

--- terminal/Microsoft.PowerShell_profile.ps1
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -41,3 +41,18 @@ function ifzf {
   ii (Split-Path -Parent (fzf))
 }
 New-Alias -Name search -Value ifzf
+
+# start hoogle server, server needs to first be generated once using:
+# `stack hoogle -- generate`
+function Start-Hoogle {
+  $portInUse = Get-NetTCPConnection -LocalPort 8080 -ErrorAction SilentlyContinue
+
+  if ($portInUse) {
+    Write-Host "Port 8080 is already in use. Skipping Hoogle server startup."
+    Start-Process "http://localhost:8080"
+  } else {
+    Start-Process "http://localhost:8080"
+    stack hoogle -- server --port=8080
+  }
+}
+New-Alias -Name hoogle -Value Start-Hoogle
