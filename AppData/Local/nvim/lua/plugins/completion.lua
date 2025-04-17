3dc93acd0ed2daab6ff78817e3ec77ed12434a24
Daniel van Opstal <dvopstal05@gmail.com>
2025-04-17 14:45:52 +0200

    show hide signature binding blink.cmp

--- AppData/Local/nvim/lua/plugins/completion.lua
+++ AppData/Local/nvim/lua/plugins/completion.lua
@@ -1,26 +1,28 @@
 return {
-    {
-        'saghen/blink.cmp',
-        dependencies = 'rafamadriz/friendly-snippets',
+	{
+		"saghen/blink.cmp",
+		dependencies = "rafamadriz/friendly-snippets",
 
-        version = '1.*',
+		version = "1.*",
 
-        opts = {
-            keymap = {
-                preset = 'enter',
+		opts = {
+			keymap = {
+				preset = "enter",
 
-                ['<C-k>'] = { 'select_prev', 'fallback' },
-                ['<C-j>'] = { 'select_next', 'fallback' },
-            },
+				["<C-k>"] = { "select_prev", "fallback" },
+				["<C-j>"] = { "select_next", "fallback" },
 
-            appearance = {
-                nerd_font_variant = 'mono'
-            },
+				["<C-n>"] = { "show_signature", "hide_signature", "fallback" },
+			},
 
-            fuzzy = {
-                implementation = "prefer_rust_with_warning"
-            },
-            signature = { enabled = true }
-        },
-    },
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+
+			fuzzy = {
+				implementation = "prefer_rust_with_warning",
+			},
+			signature = { enabled = true },
+		},
+	},
 }
