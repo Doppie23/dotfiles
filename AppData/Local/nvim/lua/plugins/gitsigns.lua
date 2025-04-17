f1880def965798f88cace7b6b0c1ba72973f1d5f
Daniel van Opstal <dvopstal05@gmail.com>
2025-04-17 13:23:37 +0200

    mini.nvim and gitsigns

---
+++ AppData/Local/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				map("n", "<leader>gh", gitsigns.preview_hunk, {
+					desc = "Preview [g]it [h]unk",
+				})
+			end,
+		},
+	},
+}
---
+++ AppData/Local/nvim/lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"echasnovski/mini.nvim",
+		version = false,
+		config = function()
+			require("mini.ai").setup()
+			require("mini.trailspace").setup()
+			require("mini.pairs").setup()
+			require("mini.surround").setup()
+		end,
+	},
+}
