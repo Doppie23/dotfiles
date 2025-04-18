3a78908cd3ec306211b690650446851680565975
Daniel van Opstal <dvopstal05@gmail.com>
2025-04-18 22:43:00 +0200

    use mini for git signs

--- AppData/Local/nvim/lua/plugins/mini.lua
+++ AppData/Local/nvim/lua/plugins/mini.lua
@@ -7,6 +7,43 @@ return {
 			require("mini.trailspace").setup()
 			require("mini.pairs").setup()
 			require("mini.surround").setup()
+			require("mini.diff").setup({
+				view = {
+					style = "sign",
+					signs = {
+						add = "┃",
+						change = "┃",
+						delete = "┃",
+					},
+				},
+				mappings = {
+					-- Apply hunks inside a visual/operator region
+					apply = "",
+
+					-- Reset hunks inside a visual/operator region
+					reset = "<leader>gr",
+
+					-- Hunk range textobject to be used inside operator
+					-- Works also in Visual mode if mapping differs from apply and reset
+					textobject = "gh",
+
+					-- Go to hunk range in corresponding direction
+					goto_first = "[H",
+					goto_prev = "[h",
+					goto_next = "]h",
+					goto_last = "]H",
+				},
+			})
 		end,
+		keys = {
+			{
+				"<leader>gh",
+				function()
+					---@diagnostic disable-next-line: missing-parameter
+					MiniDiff.toggle_overlay()
+				end,
+				desc = "Toggle git diff overlay",
+			},
+		},
 	},
 }
