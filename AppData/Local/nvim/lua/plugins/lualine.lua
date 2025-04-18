30f1d4730dc0d904897a194642a85b6d3b4bc630
Daniel van Opstal <dvopstal05@gmail.com>
2025-04-18 23:01:16 +0200

    color save indicator in lualine

--- AppData/Local/nvim/lua/plugins/lualine.lua
+++ AppData/Local/nvim/lua/plugins/lualine.lua
@@ -14,9 +14,25 @@ return {
 			},
 		},
 		config = function()
+			local function custom_filename()
+				local filename = vim.fn.expand("%:t")
+				if filename == "" then
+					filename = "[No Name]"
+				end
+
+				local modified = vim.bo.modified and " %#LualineModified#[+]" or ""
+				return filename .. modified
+			end
+			local bg = require("lualine.themes.auto").normal.c.bg
+			local hl_cmd = string.format("highlight LualineModified guifg=red guibg=%s gui=bold", bg)
+			vim.cmd(hl_cmd)
+
 			local git_blame = require("gitblame")
 			require("lualine").setup({
 				sections = {
+					lualine_c = {
+						custom_filename,
+					},
 					lualine_x = {
 						{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
 						"encoding",
