83664785caf4f84a8273f56209522ce691b39578
Daniel van Opstal <dvopstal05@gmail.com>
2025-04-18 13:53:50 +0200

    add git blame to lualine

--- AppData/Local/nvim/lua/plugins/lualine.lua
+++ AppData/Local/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,30 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {},
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			{
+				"f-person/git-blame.nvim",
+				opts = {
+					message_template = "<author> • <date>",
+					date_format = "%r",
+					message_when_not_committed = "",
+					display_virtual_text = 0,
+				},
+			},
+		},
+		config = function()
+			local git_blame = require("gitblame")
+			require("lualine").setup({
+				sections = {
+					lualine_x = {
+						{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+						"encoding",
+						"fileformat",
+						"filetype",
+					},
+				},
+			})
+		end,
 	},
 }
