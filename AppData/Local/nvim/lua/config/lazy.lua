b149b4dd554ca83399decdf0282db1f12ee7a962
Daniel van Opstal <dvopstal05@gmail.com>
2025-04-28 16:09:31 +0200

    fix keymap

--- AppData/Local/nvim/lua/config/lazy.lua
+++ AppData/Local/nvim/lua/config/lazy.lua
@@ -83,7 +83,7 @@ vim.keymap.set("n", "<esc>", function()
 end, { noremap = true, silent = true })
 
 vim.keymap.set({ "n", "v", "i" }, "<C-s>", "<cmd>write<CR>")
-vim.keymap.set({ "n", "v" }, "<leader>r", "*Ncgn")
+vim.keymap.set({ "n", "v" }, "<leader>r", "*Ncgn", { remap = true, silent = true })
 vim.keymap.set("n", "<C-_>", "gcc", { remap = true, silent = true })
 vim.keymap.set("v", "<C-_>", "gc", { remap = true, silent = true })
 
