6e51dfadfbc13f80d5aaec22058ee097d5d04dde
Daniel van Opstal <dvopstal05@gmail.com>
2025-06-14 19:24:26 +0200

    undo history

--- AppData/Local/nvim/lua/plugins/snacks.lua
+++ AppData/Local/nvim/lua/plugins/snacks.lua
@@ -291,7 +291,13 @@ return {
 			-- { "<leader>sp",      function() Snacks.picker.lazy() end,                                    desc = "Search for Plugin Spec" },
 			-- { "<leader>sq",      function() Snacks.picker.qflist() end,                                  desc = "Quickfix List" },
 			-- { "<leader>sR",      function() Snacks.picker.resume() end,                                  desc = "Resume" },
-			-- { "<leader>su",      function() Snacks.picker.undo() end,                                    desc = "Undo History" },
+			{
+				"<leader>su",
+				function()
+					Snacks.picker.undo()
+				end,
+				desc = "Undo History",
+			},
 			{
 				"<leader>uC",
 				function()
