3c9fc1c573c434cff2d3eb95fdd1f8780caaad86
Daniel van Opstal <dvopstal05@gmail.com>
2025-07-18 14:20:41 +0200

    format file bidning

--- AppData/Local/nvim/lua/plugins/formatter.lua
+++ AppData/Local/nvim/lua/plugins/formatter.lua
@@ -57,6 +57,10 @@ return {
 
 			require("conform").setup(opts)
 
+			vim.keymap.set("n", "grf", function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end, { desc = "Format current file" })
+
 			vim.api.nvim_create_user_command("FormatDisable", function(args)
 				if args.bang then
 					-- FormatDisable! will disable formatting just for this buffer
