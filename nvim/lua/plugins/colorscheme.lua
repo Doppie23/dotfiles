d5010be7e02996269e3f3567f79a4ab10a0f41d6
Daniel van Opstal <dvopstal05@gmail.com>
2025-08-29 22:32:48 +0200

    different theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,36 @@
 return {
 	{
+		"aktersnurra/no-clown-fiesta.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = false, -- Enable this to disable the bg color
+			styles = {
+				-- You can set any of the style values specified for `:h nvim_set_hl`
+				comments = {},
+				functions = {},
+				keywords = {},
+				lsp = {},
+				match_paren = {},
+				type = {},
+				variables = {},
+			},
+		},
+		config = function(_, opts)
+			require("no-clown-fiesta").setup(opts)
+
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				pattern = "no-clown-fiesta",
+				callback = function()
+					vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#373737" })
+					vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1D1D1D" })
+				end,
+			})
+
+			vim.cmd("colorscheme no-clown-fiesta")
+		end,
+	},
+	{
 		"folke/tokyonight.nvim",
 		opts = {},
 		lazy = true,
