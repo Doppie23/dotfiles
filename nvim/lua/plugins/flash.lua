67881aff277a612b01855c3640d86bce5f80d09d
Daniel van Opstal <dvopstal05@gmail.com>
2025-10-02 15:58:21 +0200

    use flash only for highlighting

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -4,51 +4,16 @@ return {
 		event = "VeryLazy",
 		opts = {
 			modes = {
-				search = {
+				char = {
 					enabled = true,
+					multi_line = false,
+					char_actions = function()
+						return {
+							[";"] = "next", -- set to `right` to always go right
+							[","] = "prev", -- set to `left` to always go left
+						}
+					end,
 				},
-			},
-		},
-		keys = {
-			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-			{
-				"S",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter",
-			},
-			{
-				"r",
-				mode = "o",
-				function()
-					require("flash").remote()
-				end,
-				desc = "Remote Flash",
-			},
-			{
-				"R",
-				mode = { "o", "x" },
-				function()
-					require("flash").treesitter_search()
-				end,
-				desc = "Treesitter Search",
-			},
-			{
-				"<c-s>",
-				mode = { "c" },
-				function()
-					require("flash").toggle()
-				end,
-				desc = "Toggle Flash Search",
 			},
 		},
 	},
