c1a86d0b89a26b5f1a0f630624be87d0e56109fd
Daniel van Opstal <dvopstal05@gmail.com>
2025-12-28 22:58:09 +0100

    nvidia drivers

--- nixos/hosts/pc/configuration.nix
+++ nixos/hosts/pc/configuration.nix
@@ -100,6 +100,46 @@
     };
     users.users.daniel.extraGroups = [ "docker" ];
 
+    # Enable OpenGL
+    hardware.graphics = {
+        enable = true;
+    };
+
+    # Load nvidia driver for Xorg and Wayland
+    services.xserver.videoDrivers = ["nvidia"];
+
+    hardware.nvidia = {
+        # Modesetting is required.
+        modesetting.enable = true;
+
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+        # of just the bare essentials.
+        powerManagement.enable = false;
+
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
+
+        # An important note to take is that the option hardware.nvidia.open
+        # should only be set to false if you have a GPU with an older
+        # architecture than Turing (older than the RTX 20-Series). Also, OBS
+        # NVENC support does not seem to work currently with the open drivers.
+        #
+        # For newer cards it is recommended by NVIDIA to use the open Drivers:
+        # https://download.nvidia.com/XFree86/Linux-x86_64/565.77/README/kernel_open.html
+        open = true;
+
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
+
+        # # Optionally, you may need to select the appropriate driver version for your specific GPU.
+        # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    programs.steam.enable = true;
+
     programs.firefox.enable = true;
 
     # Some programs need SUID wrappers, can be configured further or are
