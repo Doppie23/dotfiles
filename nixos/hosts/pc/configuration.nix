fcff01320c2da09f7b54bbab3653d20334dd21e4
Daniel van Opstal <dvopstal05@gmail.com>
2025-12-28 22:37:30 +0100

    limit generations

--- nixos/hosts/pc/configuration.nix
+++ nixos/hosts/pc/configuration.nix
@@ -24,6 +24,7 @@
                 useOSProber = true;
                 devices = [ "nodev" ];
                 efiSupport = true;
+                configurationLimit = 5;
             };
             efi.canTouchEfiVariables = true;
             timeout = 5;
